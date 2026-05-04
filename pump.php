c9fa6d8ef1df2689e7b9ed6225ff6e8ead395a0c
Jim Choy <choyjim33@gmail.com>
2026-05-04 15:23:11 -0400

    php modif

--- pump.php
+++ pump.php
@@ -1,15 +1,31 @@
 <?php
 $file = "/var/www/html/irrigationsystem/pump_command.json";
 
-$mode = $_GET["mode"] ?? null;
-$pump = $_GET["pump"] ?? null;
+$current = [
+    "mode" => "automatic"
+    "pump" => "auto"
+]
 
-$data = [
-    "mode" => $mode,
-    "pump" => $pump
-];
+if (file_exists($file)) {
+    $old = json_decode(file_get_contents($file), true);
+    if (is_array($old)) {
+        $current = array_merge($current, $old);
+    }
+}
 
-file_put_contents($file, json_encode($data));
+if (isset($_GET["mode"])) {
+    $current["mode"] = $_GET["mode"];
 
-echo "Command saved";
+    if ($_GET["mode"] == "automatic") {
+        $current["pump"] = "auto";
+    }
+}
+
+if (isset($_GET["pump"]) {
+    $current["pump"] = $_GET["pump"];
+})
+
+file_put_contents($file, json_encode($current));
+
+echo json_encode($current);
 ?>
